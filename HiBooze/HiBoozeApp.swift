e3d03e9fea58cacd82540938c2fb6586b890cb76
Frank Oftring <frank.oftring@protonmail.com>
2021-12-27 20:24:07 -0500

    Initial Commit

---
+++ HiBooze/HiBoozeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiBoozeApp.swift
+//  HiBooze
+//
+//  Created by Frank Oftring on 12/27/21.
+//
+
+import SwiftUI
+
+@main
+struct HiBoozeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
